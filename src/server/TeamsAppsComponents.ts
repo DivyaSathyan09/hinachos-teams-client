786335c6cb88efe2cc936fcd0901ec041cd76e82
DivyaSathyan09 <divya.sathyan@theelixrlabs.com>
2024-02-02 16:31:25 +0530

    Initial commit of hinachos-teams-client

---
+++ src/server/TeamsAppsComponents.ts
@@ -0,0 +1,6 @@
+// Components will be added here
+export const nonce = {}; // Do not remove!
+// Automatically added for the dashboardTab tab
+export * from "./dashboardTab/DashboardTab";
+// Automatically added for the hinachosTeamsClientBot bot
+export * from "./hinachosTeamsClientBot/HinachosTeamsClientBot";
